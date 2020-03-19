3db4b0141c2c657caef61b0541eb0437661e2329
Vikram Biwal <crayvk56@gmail.com>
2020-03-19 13:40:15 +0530

    Update

--- web/index.php
+++ web/index.php
@@ -1,35 +1,29 @@
 <?php
 
-require_once "Services_Careerjet.php" ;
+$curl = curl_init();
 
-$api = new Services_Careerjet('en_IN') ;
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://indeed-indeed.p.rapidapi.com/apigetjobs?jobkeys=java&v=2&format=json&publisher=any",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => array(
+		"x-rapidapi-host: indeed-indeed.p.rapidapi.com",
+		"x-rapidapi-key: 279d8d3289msh60b7c4ff9937c08p16f77fjsnedac47b9d0a4"
+	),
+));
 
-$result = $api->search(array( 'keywords' => 'java',
-                              'location' => 'India')
-                       ) ;
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-if ( $result->type == 'JOBS' ){
-  echo "Found ".$result->hits." jobs" ;
-  echo " on ".$result->pages." pages\n" ;
-  $jobs = $result->jobs ;
-  
-  foreach( $jobs as &$job ){
-    echo " URL:     ".$job->url."\n" ;
-    echo " TITLE:   ".$job->title."\n" ;
-    echo " LOC:     ".$job->locations."\n";
-    echo " COMPANY: ".$job->company."\n" ;
-    echo " SALARY:  ".$job->salary."\n" ;
-    echo " DATE:    ".$job->date."\n" ;
-    echo " DESC:    ".$job->description."\n" ;
-    echo "\n" ;
-  }
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
 }
-
-if ( $result->type == 'LOCATIONS' ){
-  $locations = $result->locations ;
-  foreach ( $locations as &$loc ){
-    echo $loc."\n" ;
-  }
-}
-
-?>
